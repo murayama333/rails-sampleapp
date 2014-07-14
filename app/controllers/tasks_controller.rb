63cbeff4cae896e95d3b48502bc957897240e479
murayama333 <murayama333@gmail.com>
2014-07-14 22:47:27 +0000

    step1完了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
   def index
+  	@tasks = Task.where(status: 0).order("created_at desc").all
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140714221424) do
+ActiveRecord::Schema.define(version: 20140714222106) do
 
   create_table "tasks", force: true do |t|
     t.string   "title"
     t.string   "memo"
+    t.integer  "status"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
