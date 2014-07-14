c74509c8a0387b25a103ff68af7ce5f9f3416cd5
murayama333 <murayama333@gmail.com>
2014-07-14 23:12:37 +0000

    入力フォームを実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,9 @@ class TasksController < ApplicationController
   def index
   	@tasks = Task.where(status: 0).order("created_at desc").all
   end
+
+  def new
+  	@task = Task.new
+  	@task.status = 0
+  end
 end
