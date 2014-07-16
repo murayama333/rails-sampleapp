50ff60230d2a621875ca287ef7a3646cb26e4352
murayama333 <murayama333@gmail.com>
2014-07-16 22:09:23 +0000

    2.3登録処理完了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,18 @@ class TasksController < ApplicationController
   	@task = Task.new
   	@task.status = 0
   end
+
+  def create
+	@task = Task.new(task_params)
+	if @task.save
+	  redirect_to :tasks
+ 	else
+	  render 'new'
+	end
+  end
+
+  private
+  def task_params
+	params.require(:task).permit(:title, :memo, :status)
+  end
 end
